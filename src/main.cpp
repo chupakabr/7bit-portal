56151328b33c265271db8d2eec15a945b8293f31
Alex Kremer <godexsoft@loki-2.local>
2013-07-08 12:29:48 +0100

    entrypoint code and cmake files

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+// Portal entry point
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+	// todo: magic happens here
+	// blip blop	
+	std::cout << "Successfully transferred human to Tibet" << std::endl;
+
+	return 0;
+}
